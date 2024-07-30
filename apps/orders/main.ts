65ed2324a8907d74ea586a8d2ac7d7b1e7634f16
Mayank Maheshwari <mayank.maheshwari625@gmail.com>
2024-07-30 23:57:30 +0530

    added database

--- apps/orders/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { OrdersModule } from './src/orders.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(OrdersModule);
-  await app.listen(3000);
-}
-bootstrap();
-
--- libs/comman/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './comman.module';
-export * from './comman.service';
